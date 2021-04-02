bb0a24581dcea09f7bb357ef0f88b5cf4a9eccf7
Sebastian Rollen <seb@overmu.se>
2021-04-02 10:39:57 -0400

    use kafka-settings for setup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use alpaca_data_relay::run;
+use alpaca_data_relay::{run, Settings};
 use anyhow::Result;
 use dotenv::dotenv;
+use tracing::{info, subscriber::set_global_default};
+use tracing_log::LogTracer;
+use tracing_subscriber::{filter::EnvFilter, FmtSubscriber};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let _guard = sentry::init(sentry::ClientOptions::new());
     let _ = dotenv();
-    env_logger::builder().format_timestamp_micros().init();
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+    set_global_default(subscriber).expect("Failed to set subscriber");
+    LogTracer::init().expect("Failed to set logger");
+    let settings = Settings::new()?;
+    info!("Starting alpaca-data-relay");
 
-    run().await
+    run(settings).await
 }
